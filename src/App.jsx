2a5b7911c550d61dcc79ae4c6c8dae26e784b2f1
1gory <igor.pershin.me@gmail.com>
2019-03-12 14:52:50 +0300

    init project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+export default class extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    );
+  }
+}
