0c07d8cce3c2e06ef9be9907dcf306a4e436a97b
1gory <igor.pershin.me@gmail.com>
2019-03-28 03:22:06 +0300

    change booklet image

--- src/BookletSection/index.jsx
+++ src/BookletSection/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Grid, Row, Col } from 'react-flexbox-grid';
-import img from './gal-img.png';
+import img from './booklet.png';
 
 const Wrapper = styled.section`
   padding-top: 110px;
